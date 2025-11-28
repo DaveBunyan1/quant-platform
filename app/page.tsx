6a635922f12e2a80a4ebfd5567165ecc1ba1c823
Dave Bunyan <davebunyan1@gmail.com>
2025-11-28 16:18:18 -0500

    chore: Removed unused helper functions.

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,6 @@
 
 import { useEffect, useState } from "react";
 import PriceChart from "./components/PriceChart";
-import constructDownloadUrl from "./utils/constructDownloadUrl";
-
-const queryUrl = constructDownloadUrl(
-  "BTC-USD",
-  "2025-01-01",
-  "2025-06-01",
-  "daily"
-);
-
-const data = [
-  { date: new Date("2025-01-01"), value: 10 },
-  { date: new Date("2025-01-02"), value: 20 },
-  { date: new Date("2025-01-03"), value: 15 },
-  { date: new Date("2025-01-04"), value: 30 },
-  { date: new Date("2025-01-05"), value: 25 },
-];
 
 export default function Home() {
   const [testData, setTestData] = useState<
