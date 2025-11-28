4c39944a62898ec315935737bdf51492481b2d75
Dave Bunyan <davebunyan1@gmail.com>
2025-11-28 14:28:28 -0500

    feat: Base line chart created

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import useSWR from "swr";
+import PriceChart from "./components/PriceChart";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const data = [
+  { date: new Date("2025-01-01"), value: 10 },
+  { date: new Date("2025-01-02"), value: 20 },
+  { date: new Date("2025-01-03"), value: 15 },
+  { date: new Date("2025-01-04"), value: 30 },
+  { date: new Date("2025-01-05"), value: 25 },
+];
 
 export default function Home() {
   return (
     <main className="p-6">
       <h1 className="text-2xl font-bold">Crypto vs ETF Tracker</h1>
-
+      <PriceChart data={data} />
       <div className="mt-4"></div>
     </main>
   );
